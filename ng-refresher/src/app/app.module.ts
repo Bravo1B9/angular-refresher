08abec03621b719b9a70eee96a459429c1b3b4e8
Bravo1B9 <brofe93@gmail.com>
2023-03-23 05:00:41 +0000

    Implement routing

--- ng-refresher/src/app/app.module.ts
+++ ng-refresher/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { PersonInputComponent } from './persons/person-input.component';
@@ -15,6 +16,7 @@ import { PersonsComponent } from './persons/persons.component';
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
